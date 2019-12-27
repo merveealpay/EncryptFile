6d8a3c8c271f3bfb251d75f1ce1b1bc919a1f3b6
merve <merveealpay@gmail.com>
2019-12-27 11:35:24 +0300

    first commit

---
+++ src/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"crypto/aes"    //Advanced Encryption Standard is a symmetric block cipher.
+	"crypto/cipher" //şifre paketi
+	"crypto/md5"    //hash için
+	"crypto/rand"
+	"encoding/hex" //hex, hexadecimal encoding icin kullanılan bir poket
+	"fmt"
+	"io"
+)
+
+//stringi nasıl encode'luyormusuz bakalım:
+// func main() {
+// 	word := []byte("Merve")
+// 	encodedWord := hex.EncodeToString(word)
+// 	fmt.Printf("%s\n", encodedWord) //4d65727665
+// }
+
+//hashleyelim:
+func createHash(key string) string {
+	hasher := md5.New()       //md5 nesnesi üretiyoruz ve bu hasher nesnemiz olacak.
+	hasher.Write([]byte(key)) //alacagımız key'i hasher'in write fonksiyonuna yolluyoruz
+	return hex.EncodeToString(hasher.Sum(nil))
+	//nil inititialize edilmemis value.
+}
+
+//encrypt islemi icin aes paketinden faydalanacagız.
+func encrypt(data []byte, password string) []byte {
+	block, _ := aes.NewCipher([]byte(createHash(password)))
+	//buradaki _(underscore), blank identifier olarak geciyor, bu iterator degisken
+	// bize lazım degil buna ihtiyacım yok diyoruz derleyiciye
+	//password, üstteki hashleme fonksiyonuna gonderilir ve
+	//aes paketinin icindeki newCipher ile bir sifreleme blogu(block) olusturulur
+	gcm, err := cipher.NewGCM(block)
+	//NewGCM returns the given 128-bit, block cipher wrapped in Galois Counter Mode with the standard nonce length.
+	if err != nil {
+		panic(err.Error()) //stopssss, error fırlattırıyoruz
+	}
+	//şifrelenmiş bir text olusturmadan önce, bir nonce olusturmaliyiz.
+	nonce := make([]byte, gcm.NonceSize())
+	//nonce anasifreyi güvenli bir sekilde iletmek icin kullanılan,rastgele secilen bir kod
+	//her transactionda nonce value 1 artar.
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		panic(err.Error())
+	}
+	//decryption için kullanacagımız nonce, encryption icin kullanılanla aynı olmalı!
+	ciphertext := gcm.Seal(nonce, nonce, data, nil)
+	//ilk parametre bizim prefix value, encrypted olan dataya bu eklenecek
+	//nonce, NonceSize() byte uzunlguunda ve unique olmalı her zaman,verilen key icin.
+	//Seal burda encrypt ediyor
+	return ciphertext
+
+}
+
+func decrypt(data []byte, password string) []byte {
+	key := []byte(createHash(password))
+	//encrypted olmus datayı decrypt etmek istiyoruz,bu islem aslında cok benziyor encryption islemine.
+	//hashlenmis password  kullanarak yeni bir sifre block u olusturuyoruz.
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err.Error())
+	}
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+	nonceSize := gcm.NonceSize()
+	//encrypt fonksiyonunda nonce ile datayı encrpyt ederken prefix olarak kullanmıstık,
+	//simdi burda nonce ve encrypt olmus datayı ayırmamız gerek.
+	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
+	//nonce ve ciphertext birbirinden ayrıldı, biz simdi decrypt edebiliriz datayı
+	// ve onu bir plaintext olarak return edebiliriz.
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		panic(err.Error())
+	}
+	return plaintext
+}
+
+func main() {
+	ciphertext := encrypt([]byte("mervvee"), "password") // encrypt fonksiyonunu cagırarak, sifreyi olusturuyoruz.
+	fmt.Printf("Encrypted: %x\n", ciphertext)            //  (%x :base 16, with lower-case letters for a-f)
+	plaintext := decrypt(ciphertext, "password")         //decrypt edilmis hali, plaintext.
+	fmt.Printf("Decrypted: %s\n", plaintext)
+
+}
